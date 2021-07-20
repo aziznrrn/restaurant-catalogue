76df75c5411a835bffcdcde35e7f63011e7f2a23
aziz <azizrn38@gmail.com>
2021-07-20 16:55:42 +0700

    equalize banner height

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -5,20 +5,22 @@ const Home = {
   async render () {
     return `
       <div class="jumbotron">
-        <picture>
-          <source media="(max-width: 420px)" srcset="/images/hero_small.webp"
-                  style="width: 100%; height: 100%; object-fit: cover">
-          <source media="(max-width: 420px)" srcset="/images/hero_small.jpg"
-                  style="width: 100%; height: 100%; object-fit: cover">
-          <source media="(max-width: 800px)" srcset="/images/hero_medium.webp"
-                  style="width: 100%; height: 100%; object-fit: cover">
-          <source media="(max-width: 800px)" srcset="/images/hero_medium.webp"
-                  style="width: 100%; height: 100%; object-fit: cover">
-          <source srcset="/images/hero.webp"
-                  style="width: 100%; height: 100%; object-fit: cover">
-          <img src="/images/hero.jpg" alt="banner"
-                  style="width: 100%; height: 100%; object-fit: cover">
-        </picture>
+        <div class="banner">
+          <picture class="banner">
+            <source media="(max-width: 420px)" srcset="/images/hero_small.webp"
+                    style="width: 100%; height: 100%; object-fit: cover">
+            <source media="(max-width: 420px)" srcset="/images/hero_small.jpg"
+                    style="width: 100%; height: 100%; object-fit: cover">
+            <source media="(max-width: 800px)" srcset="/images/hero_medium.webp"
+                    style="width: 100%; height: 100%; object-fit: cover">
+            <source media="(max-width: 800px)" srcset="/images/hero_medium.webp"
+                    style="width: 100%; height: 100%; object-fit: cover">
+            <source srcset="/images/hero.webp"
+                    style="width: 100%; height: 100%; object-fit: cover">
+            <img src="/images/hero.jpg" alt="banner"
+                    style="width: 100%; height: 100%; object-fit: cover">
+          </picture>
+        </div>
         <h1 class="jumbotron__text">
           Eat Well, <em>Live Well</em>
         </h1>
